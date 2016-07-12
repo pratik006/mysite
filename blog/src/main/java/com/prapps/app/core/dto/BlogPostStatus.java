e96a3ca7d71387675e2b7b9fa2561b88ce056c00
Pratik Sengupta <pratik006@gmail.com>
2016-07-12 22:03:34 +0530

    forgot adding enum for create draft post

---
+++ blog/src/main/java/com/prapps/app/core/dto/BlogPostStatus.java
@@ -0,0 +1,31 @@
+package com.prapps.app.core.dto;
+
+public enum BlogPostStatus {
+
+	COMPLETE("COMPLETE"),
+	DRAFT("DRAFT");
+	
+	private String code;
+	
+	private BlogPostStatus(String code) {
+		this.code = code;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public static BlogPostStatus getByCode(String code) {
+		for (BlogPostStatus value : BlogPostStatus.values()) {
+			if (value.code.equalsIgnoreCase(code)) {
+				return value;
+			}
+		}
+		
+		return null;
+	}
+}
