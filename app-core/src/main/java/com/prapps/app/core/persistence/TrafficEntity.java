d6c211c31cda96594e6afd922edf73cecb391b83
Pratik Sengupta <pratik006@gmail.com>
2017-03-04 14:20:56 +0530

    reverting to older hiubernate version as hibernate5 has issue with multiple schemas

--- app-core/src/main/java/com/prapps/app/core/persistence/TrafficEntity.java
+++ app-core/src/main/java/com/prapps/app/core/persistence/TrafficEntity.java
@@ -12,7 +12,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name="TRAFFIC")
+@Table(name="traffic")
 public class TrafficEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 
