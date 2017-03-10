20de0dc5b74cac68ac31e1e4e092ceecd67a0400
Pratik Sengupta <pratik006@gmail.com>
2017-03-10 22:37:42 +0530

    added keepalive

--- app-core/src/main/java/com/prapps/app/core/controller/CoreController.java
+++ app-core/src/main/java/com/prapps/app/core/controller/CoreController.java
@@ -1,9 +1,14 @@
 package com.prapps.app.core.controller;
 
+import java.net.URL;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -17,15 +22,41 @@ import com.prapps.app.core.dto.User;
 
 @Controller
 @ControllerAdvice
+@EnableAsync
 @RequestMapping("/rest")
 public class CoreController {
 
 	private UserService userService;
+	private static boolean keepAlive = false;
 	
 	@Inject
 	public CoreController(UserService userService) {
 		this.userService = userService;
 	}
+	
+	@Async
+	private void start(String url) {
+		if (!keepAlive) {
+			keepAlive = true;
+			while(keepAlive) {
+				try {
+					System.out.println(url);
+					new URL(url).openConnection();
+					Thread.sleep(1000*60*60*12);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	}
+	
+	@RequestMapping(value = "/keepAlive", method = RequestMethod.GET)
+	@Consumes(value=  MediaType.APPLICATION_JSON)
+    public @ResponseBody String keepAlive(HttpServletRequest request) {
+		start(request.getRequestURL().toString());
+		return "alive";
+    }
 	
 	@RequestMapping(value = "/userinfo", method = RequestMethod.GET)
 	@Consumes(value=  MediaType.APPLICATION_JSON)
