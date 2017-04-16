a3f0e0c01c495c7a05ea034434cfd5a97b01e9dd
Pratik Sengupta <pratik006@gmail.com>
2017-04-16 12:25:25 +0530

    added accordion

--- app-ui/src/main/resources/static/rail/script.js
+++ app-ui/src/main/resources/static/rail/script.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
 	var stations;
 	var REGION = "hyd-mmts";
 	var CONTEXT = "http://apps-pratiks.rhcloud.com/rest/rail/";
+	//var CONTEXT = "http://localhost:8080/rest/rail/";
+	//var CONTEXT = "http://192.168.0.102:8080/rest/rail/";
+	if(document.querySelector('.mdl-layout').MaterialLayout) {
+		document.querySelector('.mdl-layout').MaterialLayout.drawerToggleHandler_();
+		//$('.mdl-layout').addClass('drawer-collapsed-width');
+	}
 
     var from = $(".typeahead");
     $.get(CONTEXT+"stations?region=hyd-mmts", function(data){
@@ -44,6 +50,7 @@ $(document).ready(function() {
 			
 			$("#timetable > table > tbody > tr").remove();
 			$.get(url, function(trains) {
+
 				document.querySelector('.mdl-layout').MaterialLayout.drawerToggleHandler_();
 				$("#timetable").show();
 				if (trains.length == 0) {
@@ -58,14 +65,74 @@ $(document).ready(function() {
 
 				trains.forEach(function(train) {
 					$("#timetable > table > tbody").append(
-						"<tr><td class='mdl-cell--2 no-padding' style='padding-left: 0px !'>"+train.id+"</td><td>"+train.name+"</td>"
-						+"<td>"+train.routes[0].arrival+"</td>"
-						+"<td class=''>"+train.routes[1].arrival+"</td>"
-						+"<td class='mdl-cell--hide-phone'>"+train.trainType+"</td>"
+						"<tr class='accordion'>"
+						+"<td class='no-padding icon-width'>"
+							+"<button class='no-padding mdl-button mdl-js-button mdl-micron-button mdl-button--colored mdl-js-ripple-effect'>"
+							+"<i class='material-icons'>add</i></button>"
+						+"</td>"
+						+"<td class='mdl-data-table__cell--non-numeric train-number'>"+train.id+"</td>"
+						+"<td class='mdl-data-table__cell--non-numeric train-name'>"+train.name+"</td>"
+						+"<td class='mdl-layout--large-screen-only mdl-cell--hide-phone'>"+train.routes[0].arrival+"</td>"
+						+"<td class='mdl-data-table__cell--non-numeric'>"+train.routes[0].departure+"</td>"
+						+"<td class='mdl-layout--large-screen-only mdl-cell--hide-phone'>"+train.trainType+"</td>"
+						+"</tr>"
+						+"<tr class='accordion-panel'>"
+						+"<td class='mdl-cell--5 no-padding' colspan='5'>"+buildRouteGrid(train.routes)+"</td>"
 						+"</tr>");
 				});
+				
+				$("tr.accordion").click(function() {
+					console.log('clicked');
+					/* Toggle between adding and removing the "active" class,
+			        to highlight the button that controls the panel */
+			        this.classList.toggle("active");
+
+
+			        /* Toggle between hiding and showing the active panel */
+			        var panel = this.nextElementSibling;
+			        if (panel.style.display === "table-row") {
+			            panel.style.display = "none";
+			            $(this).find("i").html("add");
+			        } else {
+			            panel.style.display = "table-row";
+			            $(this).find("i").html("remove");
+			        }
+
+
+			        
+				});
+				
 			});
 		}
 
 	});
+
+	function buildRouteGrid(routes) {
+		var html = "<div class='text-center'><span class='mdl-layout-title minor-padding'>Route Information</span></div>" 
+		+"<div id='route'>"
+		  +"<table class='mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp full-width'>"
+		    +"<thead>"
+		      +"<tr>"
+		        +"<th class='mdl-data-table__cell--non-numeric mdl-cell--1-col'>Station</th>"
+		        +"<th class='mdl-data-table__cell'>Arrival</th>"
+		        +"<th class='mdl-data-table__cell'>Departure</th>"
+		        +"<th class='mdl-data-table__cell mdl-cell--hide-phone'>Distance</th>"
+		      +"</tr>"
+		    +"</thead>";
+		routes.forEach(function(route) {
+			html += "<tr>"
+		        +"<td class='mdl-data-table__cell--non-numeric mdl-cell--1-col'>"+route.stationName +"</td>"
+		        +"<td class='mdl-data-table__cell'>"+route.arrival+"</td>"
+		        +"<td class='mdl-data-table__cell'>"+route.departure+"</td>"
+		        +"<td class='mdl-data-table__cell mdl-cell--hide-phone'>"+route.dist+"</td>"
+		      +"</tr>";
+		});
+
+
+		html+="<tbody>"
+		    +"</tbody>"
+		  +"</table>"
+		+"</div>";
+		return html;
+	}
 });
