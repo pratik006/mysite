6466b96862d1e448ed6b693c43617cfe4add349e
Pratik Sengupta <pratik006@gmail.com>
2017-06-18 20:55:41 +0530

    added sql backup scripts

---
+++ scripts/export_db_script.sh
@@ -0,0 +1,9 @@
+
+# password is 642bYspZ-FGX
+/usr/bin/mysqldump -u adminXbfDGyd -p -h 127.0.0.1 -P 55051 --databases  apps > ~/scripts.sql 
+
+
+
+# fro mlocal
+/usr/bin/mysqldump -u root -p -h 127.0.0.1 -P 3306 --databases  apps > ~/app-scripts.sql 
+/usr/bin/mysqldump -u root -p -h 127.0.0.1 -P 3306 --databases  trainapp > ~/trainapp-scripts.sql 
